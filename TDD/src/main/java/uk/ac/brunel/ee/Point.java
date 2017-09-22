d551cf245fed51d147db1ecde91b0f6d527d316a
Chris <C.Gschrey@gmx.net>
2017-09-22 09:47:58 +0200

    Implementierung der Point-Klasse

--- TDD/src/main/java/uk/ac/brunel/ee/Point.java
+++ TDD/src/main/java/uk/ac/brunel/ee/Point.java
@@ -1,17 +1,21 @@
 package uk.ac.brunel.ee;
 
+import java.util.Objects;
+
 public class Point {
 	private double x;
 	private double y;
-	
-	public Point(){
-		// TODO
+
+	public Point() {
+		this.x = 0.0;
+		this.y = 0.0;
 	}
 
-	public Point(double x, double y){
-		// TODO
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
-	
+
 	public double getX() {
 		return x;
 	}
@@ -27,34 +31,51 @@ public class Point {
 	public void setY(double y) {
 		this.y = y;
 	}
-	
+
 	@Override
-	public boolean equals(Object o){
-		//TODO
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+		if (o instanceof Point) {
+			Point p = (Point) o;
+			return (p.x == this.x && p.y == this.y);
+		}
 		return false;
 	}
-	
+
 	@Override
-	public int hashCode(){
-		//TODO
-		return 0;
+	public int hashCode() {
+		return Objects.hash(this.x, this.y);
 	}
-	
+
 	@Override
-	public String toString(){
-		//TODO
-		return "";
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("( ");
+		stringBuilder.append(this.x);
+		stringBuilder.append(", ");
+		stringBuilder.append(this.y);
+		stringBuilder.append(" )");
+		return stringBuilder.toString();
 	}
-	
-	public double norm(){
-		return 0.0;
+
+	public double norm() {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
 	}
-	
-	public void rotate(double theta){
-		//TODO
+
+	public void rotate(double theta) throws AngleOutOfRangeException {
+		if (theta < -180.0 || theta > 180.0) {
+			throw new AngleOutOfRangeException();
+		}
+		double newX = this.x * Math.cos(theta) - this.y * Math.sin(theta);
+		double newY = this.x * Math.sin(theta) + this.y * Math.cos(theta);
+		this.x = newX;
+		this.y = newY;
 	}
-	
-	public void displace(Point p){
-		//TODO
+
+	public void displace(Point p) {
+		this.x+=p.x;
+		this.y+=p.y;
 	}
 }
