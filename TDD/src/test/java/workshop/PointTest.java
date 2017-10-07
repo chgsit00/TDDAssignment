14efd29a7b83c0c21a9c595276aada5fe4b378d4
MatzeG <matthiasgebert2006@web.de>
2017-10-07 17:05:00 +0200

    update pointtest

--- TDD/src/test/java/workshop/PointTest.java
+++ TDD/src/test/java/workshop/PointTest.java
@@ -11,17 +11,26 @@ public class PointTest {
 	@Test
 	public void testDefaultConstructor() {
 		Point point = new Point();
-		Assert.assertEquals(0.0, point.getX(), 0);
-		Assert.assertEquals(0.0, point.getY(), 0);
+		Assert.assertEquals(0, point.getX(), 0);
+		Assert.assertEquals(0, point.getY(), 0);
 	}
 
 	// ------------------
-	// Default Constructor Tests
+	// Alternate Constructor Tests
 	// ------------------
 	@Test
-	public void testAlternateConstructor() {
+	public void testAlternateConstructorPositiveValues() {
 		double x = 1.7;
 		double y = 2.7;
+		Point point = new Point(x, y);
+		Assert.assertEquals(x, point.getX(), 0);
+		Assert.assertEquals(y, point.getY(), 0);
+	}
+
+	@Test
+	public void testAlternateConstructorNegativeValues() {
+		double x = -1.7;
+		double y = -2.7;
 		Point point = new Point(x, y);
 		Assert.assertEquals(x, point.getX(), 0);
 		Assert.assertEquals(y, point.getY(), 0);
@@ -52,11 +61,11 @@ public class PointTest {
 	// Equals Test
 	// ------------------
 	@Test
-	public void testEqualsSamePoints() {
+	public void testEqualsSamePoint() {
 		Point point1 = new Point(1.7, 2.7);
 		Assert.assertTrue(point1.equals(point1));
 	}
-	
+
 	@Test
 	public void testEqualsDifferentPointsWithSameCoordinates() {
 		Point point1 = new Point(1.7, 2.7);
@@ -76,6 +85,12 @@ public class PointTest {
 		Point point1 = new Point(1.7, 3.7);
 		Point point2 = null;
 		Assert.assertFalse(point1.equals(point2));
+	}
+
+	@Test
+	public void testEqualsWithOtherObject() {
+		Point point1 = new Point(1.7, 3.7);
+		Assert.assertFalse(point1.equals(""));
 	}
 
 	// ------------------
@@ -99,30 +114,42 @@ public class PointTest {
 	// ToString Test
 	// ------------------
 	@Test
-	public void testToString() {
-		Point point = new Point(1.7, 2.7);
-		Assert.assertEquals("test the toString-Methode", "( +1.7000E+00, +2.7000E+00 )", point.toString());
+	public void testToStringDefaultPoint() {
+		Point point = new Point();
+		Assert.assertEquals("test the toString-Methode", "( +0.0000E+00, +0.0000E+00 )", point.toString());
+	}
+
+	@Test
+	public void testToStringWithPositiveValues() {
+		Point point = new Point(0.0000065342, 4.23454);
+		Assert.assertEquals("test the toString-Methode", "( +6.5342E-06, +4.2345E+00 )", point.toString());
+	}
+
+	@Test
+	public void testToStringWitNegativeValues() {
+		Point point = new Point(-0.0000065342, -4.23454);
+		Assert.assertEquals("test the toString-Methode", "( -6.5342E-06, -4.2345E+00 )", point.toString());
 	}
 
 	// ------------------
 	// Norm Test
 	// ------------------
 	@Test
-	public void testNorm() {
+	public void testNormWithDefaultPoint() {
+		Point point = new Point();
+		double distance = 0;
+		Assert.assertEquals(distance, point.norm(), 0);
+	}
+
+	@Test
+	public void testNormWithTwoPositive() {
 		Point point = new Point(1.7, 2.7);
 		double distance = 3.1906112267087634;
 		Assert.assertEquals(distance, point.norm(), 0);
 	}
 
 	@Test
-	public void testNormWithOneMinus() {
-		Point point = new Point(-1.7, 2.7);
-		double distance = 3.1906112267087634;
-		Assert.assertEquals(distance, point.norm(), 0);
-	}
-
-	@Test
-	public void testNormWithTwoMinus() {
+	public void testNormWithTwoNegative() {
 		Point point = new Point(-1.7, -2.7);
 		double distance = 3.1906112267087634;
 		Assert.assertEquals(distance, point.norm(), 0);
@@ -132,15 +159,23 @@ public class PointTest {
 	// Rotate Tests
 	// ------------------
 	@Test(expected = AngleOutOfRangeException.class)
-	public void testRotateWithException() throws AngleOutOfRangeException {
+	public void testRotateWithExceptionPositive() throws AngleOutOfRangeException {
 		Point point = new Point(3.5, 0);
 		// max rotation is +/-180 above/under that an AngleOutOfRangeException should
 		// occur.
 		point.rotate(200);
 	}
-
+	
+	@Test(expected = AngleOutOfRangeException.class)
+	public void testRotateWithExceptionNegative() throws AngleOutOfRangeException {
+		Point point = new Point(3.5, 0);
+		// max rotation is +/-180 above/under that an AngleOutOfRangeException should
+		// occur.
+		point.rotate(-200);
+	}
+	
 	@Test
-	public void testRotatePlus90() throws AngleOutOfRangeException {
+	public void testRotateCounterClockWise() throws AngleOutOfRangeException {
 		Point point = new Point(3.5, 0);
 		point.rotate(90);
 		double x = 0;
@@ -150,7 +185,7 @@ public class PointTest {
 	}
 
 	@Test
-	public void testRotateMinus90() throws AngleOutOfRangeException {
+	public void testRotateClockWise() throws AngleOutOfRangeException {
 		Point point = new Point(3.5, 0);
 		point.rotate(-90);
 		double x = 0;
@@ -163,45 +198,25 @@ public class PointTest {
 	// Displace Tests
 	// ------------------
 	@Test
-	public void testDisplaceRight() {
+	public void testDisplaceWithoutChange() {
 		Point point = new Point(1.7, 2.7);
-		point.displace(new Point(1.3, 0));
-		double x = 3;
+		point.displace(new Point());
+		double x = 1.7;
 		double y = 2.7;
 		Assert.assertEquals(Math.ulp(x), Math.ulp(point.getX()), 0);
 		Assert.assertEquals(Math.ulp(y), Math.ulp(point.getY()), 0);
 	}
-
+	
 	@Test
-	public void testDisplaceLeft() {
+	public void testDisplaceWithNull() {
 		Point point = new Point(1.7, 2.7);
-		point.displace(new Point(-1.3, 0));
-		double x = 0.4;
+		point.displace(null);
+		double x = 1.7;
 		double y = 2.7;
 		Assert.assertEquals(Math.ulp(x), Math.ulp(point.getX()), 0);
 		Assert.assertEquals(Math.ulp(y), Math.ulp(point.getY()), 0);
 	}
-
-	@Test
-	public void testDisplaceTop() {
-		Point point = new Point(1.7, 2.7);
-		point.displace(new Point(0, 2.3));
-		double x = 1.7;
-		double y = 5;
-		Assert.assertEquals(Math.ulp(x), Math.ulp(point.getX()), 0);
-		Assert.assertEquals(Math.ulp(y), Math.ulp(point.getY()), 0);
-	}
-
-	@Test
-	public void testDisplaceDown() {
-		Point point = new Point(1.7, 2.7);
-		point.displace(new Point(0, -2.3));
-		double x = 1.7;
-		double y = 0.4;
-		Assert.assertEquals(Math.ulp(x), Math.ulp(point.getX()), 0);
-		Assert.assertEquals(Math.ulp(y), Math.ulp(point.getY()), 0);
-	}
-
+	
 	@Test
 	public void testDisplaceTopRight() {
 		Point point = new Point(1.7, 2.7);
@@ -218,16 +233,6 @@ public class PointTest {
 		point.displace(new Point(-1.3, -2.3));
 		double x = 0.4;
 		double y = 0.4;
-		Assert.assertEquals(Math.ulp(x), Math.ulp(point.getX()), 0);
-		Assert.assertEquals(Math.ulp(y), Math.ulp(point.getY()), 0);
-	}
-
-	@Test
-	public void testDisplaceWithoutChange() {
-		Point point = new Point(1.7, 2.7);
-		point.displace(new Point(0, 0));
-		double x = 1.7;
-		double y = 2.7;
 		Assert.assertEquals(Math.ulp(x), Math.ulp(point.getX()), 0);
 		Assert.assertEquals(Math.ulp(y), Math.ulp(point.getY()), 0);
 	}
